52732c20b6dce842592d42e6bda3e3ffeca7a253
Shefeek Jinnah <shefeek.jinnah@thoughtspot.com>
2025-11-14 10:43:27 +0000

    [Mode] Create Report 'pusheen_demo'

---
+++ Mode/nugget_poc_test/spaces/NuggetPocTest/pusheen_demo.78ebf031b2cd/Query 1.cd305e88980a.sql
@@ -0,0 +1,13 @@
+/*Committing from pusheen demo*/
+  SELECT 
+		collection_name,
+		collection_token,
+		data_updated_at,
+		dataset_name,
+		dataset_token,
+		table_bytes_used,
+		table_name,
+		table_schema,
+		table_updated_at,
+		visible
+ FROM ts_info.tables LIMIT 100;
